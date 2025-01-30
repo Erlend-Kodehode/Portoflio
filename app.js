232179f2a84dd783ac2d5e6a8f3318a066675245
Erlend <erlendk.kodehode@gmail.com>
2025-01-30 14:49:44 +0100

    more styling

--- app.js
+++ app.js
@@ -14,18 +14,20 @@ const projectContainer = document.querySelector("#project-container");
 projects.forEach(({ image, title, desc, tech }) => {
   const projectBox = document.createElement("div");
   const projectImg = document.createElement("img");
-  projectImg.src = image;
+  projectImg.src = `./images/projects/${image}`;
   const projectInfo = document.createElement("div");
-  const projectTitle = document.createElement("p");
+  const projectTitle = document.createElement("h3");
   projectTitle.textContent = title;
   const projectDesc = document.createElement("p");
   projectDesc.textContent = desc;
+  projectInfo.classList.add("project-desc");
   const techContainer = document.createElement("div");
   tech.forEach((icon) => {
     const techIcon = document.createElement("img");
     techIcon.src = `./images/${icon}`;
     techContainer.append(techIcon);
   });
+  techContainer.classList.add("tech-container");
   projectInfo.append(projectTitle, projectDesc);
   projectBox.append(projectImg, projectInfo, techContainer);
   projectContainer.append(projectBox);
--- projects.js
+++ projects.js
@@ -1,7 +1,7 @@
 export default projects = [
   {
     title: "Drumkit",
-    image: "",
+    image: "Drumkit.png",
     desc: "En enkel drumkit med gjenbindbare knapper laget i Javascript",
     tech: [
       "vscode-icons--file-type-html.svg",
@@ -11,13 +11,13 @@ export default projects = [
   },
   {
     title: "Møbel nettside",
-    image: "",
+    image: "Furniture-Website.png",
     desc: "En fiksjonell møbel nettside designet i Figma",
     tech: ["devicon--figma.svg"],
   },
   {
     title: "Hyrule Compendium",
-    image: "",
+    image: "Hyrule-compendium.png",
     desc: "Viser informasjon om objekter fra spillet: The Legend of Zelda: Breath of the Wild ved hjelp av en Api",
     tech: [
       "vscode-icons--file-type-html.svg",
@@ -27,7 +27,7 @@ export default projects = [
   },
   {
     title: "Ønskelister for Julenissen",
-    image: "",
+    image: "Wishlists.png",
     desc: "En visning av ønskelister skrevet til julenissen laget i React",
     tech: [
       "vscode-icons--file-type-html.svg",
