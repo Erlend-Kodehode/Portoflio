936bcd6c5a3dc7ae86ee81e9189e6b7a8c924f9c
Erlend-Kodehode <erlendk.kodehode@gmail.com>
2025-02-03 04:38:00 +0100

    mostly done

--- app.js
+++ app.js
@@ -11,15 +11,52 @@ window.addEventListener("scroll", () => {
 });
 
 const projectContainer = document.querySelector("#project-container");
-projects.forEach(({ image, title, desc, tech }) => {
+projects.forEach(({ image, title, desc, tech, links }) => {
   const projectBox = document.createElement("div");
+  const projectHover = document.createElement("div");
+  projectBox.addEventListener("mouseenter", () =>
+    projectHover.classList.add("project-hover")
+  );
+  projectBox.addEventListener("mouseleave", () =>
+    projectHover.classList.remove("project-hover")
+  );
+
+  if (links.figma) {
+    const figmaIcon = document.createElement("img");
+    figmaIcon.src = "./images/devicon--figma.svg";
+    const figmaBtn = document.createElement("a");
+    figmaBtn.href = links.figma;
+    figmaBtn.textContent = "Figma";
+    figmaBtn.prepend(figmaIcon);
+    figmaBtn.classList.add("main-font");
+    projectHover.append(figmaBtn);
+  } else {
+    const gitRepIcon = document.createElement("img");
+    gitRepIcon.src = "./images/mdi--github.svg";
+    const gitRepBtn = document.createElement("a");
+    gitRepBtn.href = links.rep;
+    gitRepBtn.textContent = "Repository";
+    gitRepBtn.prepend(gitRepIcon);
+    gitRepBtn.classList.add("main-font");
+    const gitPagesIcon = document.createElement("img");
+    gitPagesIcon.src = "./images/mdi--github.svg";
+    const gitPagesBtn = document.createElement("a");
+    gitPagesBtn.href = links.pages;
+    gitPagesBtn.textContent = "Pages";
+    gitPagesBtn.prepend(gitPagesIcon);
+    gitPagesBtn.classList.add("main-font");
+    projectHover.append(gitRepBtn, gitPagesBtn);
+  }
+
   const projectImg = document.createElement("img");
   projectImg.src = `./images/projects/${image}`;
   const projectInfo = document.createElement("div");
   const projectTitle = document.createElement("h3");
   projectTitle.textContent = title;
+  projectTitle.classList.add("main-font");
   const projectDesc = document.createElement("p");
   projectDesc.textContent = desc;
+  projectDesc.classList.add("main-font");
   projectInfo.classList.add("project-desc");
   const techContainer = document.createElement("div");
   tech.forEach((icon) => {
@@ -29,6 +66,28 @@ projects.forEach(({ image, title, desc, tech }) => {
   });
   techContainer.classList.add("tech-container");
   projectInfo.append(projectTitle, projectDesc);
-  projectBox.append(projectImg, projectInfo, techContainer);
+  // projectHover.append(links.figma ? figmaBtn : (gitRepBtn, gitPagesBtn));
+  projectBox.append(projectImg, projectInfo, techContainer, projectHover);
   projectContainer.append(projectBox);
 });
+
+const conactBtn = document.querySelector("#contact-btn");
+const contactInfo = document.querySelector("#contact-info");
+let hoveringLink = false;
+
+conactBtn.addEventListener("click", () => {
+  contactInfo.showModal();
+  document.body.addEventListener("mousedown", closeModal);
+});
+function closeModal() {
+  if (hoveringLink) return;
+  document.body.removeEventListener("mousedown", closeModal);
+  contactInfo.close();
+}
+
+const links = document.getElementsByClassName("contact-link");
+
+for (const link of links) {
+  link.addEventListener("mouseenter", () => (hoveringLink = true));
+  link.addEventListener("mouseleave", () => (hoveringLink = false));
+}
--- projects.js
+++ projects.js
@@ -8,12 +8,20 @@ export default projects = [
       "devicon--css3.svg",
       "skill-icons--javascript.svg",
     ],
+    links: {
+      rep: "https://github.com/Erlend-Kodehode/drumkit-assignment",
+      pages: "https://erlend-kodehode.github.io/drumkit-assignment/",
+    },
   },
   {
     title: "Møbel nettside",
     image: "Furniture-Website.png",
     desc: "En fiksjonell møbel nettside designet i Figma",
     tech: ["devicon--figma.svg"],
+    links: {
+      figma:
+        "https://www.figma.com/design/zjKCZInutX8OEv7wLd7SS3/Figma-Advanced?node-id=0-1&t=LTRRNMISG9MBBEk9-1",
+    },
   },
   {
     title: "Hyrule Compendium",
@@ -24,6 +32,10 @@ export default projects = [
       "devicon--css3.svg",
       "skill-icons--javascript.svg",
     ],
+    links: {
+      rep: "https://github.com/Erlend-Kodehode/Hyrule-Compendium",
+      pages: "https://erlend-kodehode.github.io/Hyrule-Compendium/",
+    },
   },
   {
     title: "Ønskelister for Julenissen",
@@ -36,6 +48,10 @@ export default projects = [
       "devicon--react.svg",
       "devicon--figma.svg",
     ],
+    links: {
+      rep: "https://github.com/Erlend-Kodehode/React-Prosjekt-Onskelister",
+      pages: "https://erlend-kodehode.github.io/React-Prosjekt-Onskelister/",
+    },
   },
   /*   {
       title: "",
